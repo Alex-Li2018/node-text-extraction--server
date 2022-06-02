537986de827c7597c7583e6bb0bc8194cd811d0c
Alex-Li2018 <392064881@qq.com>
2022-06-02 16:10:36 +0800

    feat: 暂时只支持纯文本

--- app/service/text-parse/test.ts
+++ app/service/text-parse/test.ts
@@ -2,6 +2,6 @@ import { praseAsync } from './docx-parse';
 import * as fs from 'fs';
 import * as path from 'path';
 
-praseAsync(fs.readFileSync(path.resolve(__dirname, './test.docx')), {}).then(res => {
+praseAsync(fs.readFileSync(path.resolve(__dirname, './test11.docx')), {}).then(res => {
   console.log(res);
 });
